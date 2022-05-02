9cf78fb829e1b3abdabc1a42e0d914255cbfb2e0
Neel Patel <51558305+patel-neel@users.noreply.github.com>
2022-05-02 10:57:39 -0400

    Time: 161 ms (17.82%), Space: 47.7 MB (20.08%) - LeetHub

---
+++ 136-single-number/136-single-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const singleNumber = function(nums) {
+    const ht = {};
+    for (const num of nums){
+        ht[num] = ht[num] + 1 || 1;
+    }
+    for(const key in ht){
+        if(ht[key] === 1){
+            return key
+        }
+    }
+};
