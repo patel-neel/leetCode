cd009c3600a0a5f4c2e6ad92c3608e653c4fc65c
Neel Patel <51558305+patel-neel@users.noreply.github.com>
2022-05-03 00:06:17 -0400

    Time: 111 ms (48.53%), Space: 51.7 MB (62.32%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let profit = 0;
+    let lowestPrice = Infinity;
+    for (let i = 0; i < prices.length; i++){
+        lowestPrice = Math.min(lowestPrice, prices[i]);
+        profit = Math.max(profit, prices[i] - lowestPrice)
+    }
+    return profit;
+};
